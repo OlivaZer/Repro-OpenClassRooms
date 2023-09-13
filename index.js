199d6dfe039d1734290760bca6ac73462a1136a3
Oliva Florian <olivaxv@gmail.com>
2023-09-13 11:49:31 +0200

    création fonction js

---
+++ index.js
@@ -0,0 +1,23 @@
+// Cette fonction "previous" est appelée lorsqu'un utilisateur souhaite faire défiler vers la gauche.
+function previous() {
+    // On récupère la largeur de l'élément avec la classe CSS "div-slider1".
+    const widthSlider = document.querySelector('.div-slider1').offsetWidth;
+
+    // On déplace la barre de défilement horizontal de l'élément avec la classe CSS "all-slider"
+    // vers la gauche en soustrayant la largeur de "div-slider1".
+    document.querySelector('.all-slider').scrollLeft -= widthSlider;
+}
+
+// Cette fonction "next" est appelée lorsqu'un utilisateur souhaite faire défiler vers la droite.
+function next() {
+    // On récupère la largeur de l'élément avec la classe CSS "div-slider1".
+    const widthSlider = document.querySelector('.div-slider1').offsetWidth;
+
+    // On déplace la barre de défilement horizontal de l'élément avec la classe CSS "all-slider"
+    // vers la droite en ajoutant la largeur de "div-slider1".
+    document.querySelector('.all-slider').scrollLeft += widthSlider;
+}
+
+
+
+
